0528e2471e9161b080add16858d90a1895795c8a
Joao Kicunga <https://github.com/KicungaTI97/Ignite_Timer>
2024-04-22 00:23:16 +0100

    Estrutura da aplicação - Cores & fonte

--- src/styles/themes/global.ts
+++ src/styles/themes/global.ts
@@ -6,8 +6,22 @@ export const GlobalStyle = createGlobalStyle`
         padding: 0;
         box-sizing: border-box;
     }
-    body{
-        background-color: #333;
-        color: #fff;
+
+    :focos{
+        outline: 0;
+        box-shadow: 0 0 0 2px ${props => props.theme['gray-500']};
     }
+
+    body{
+        background-color: ${props => props.theme['gray-900']};
+        color: ${props => props.theme['gray-300']};
+    }
+
+    body, input, textarea, button{
+        font-family: 'Roboto', sans-serif;
+        font-weight: 400;
+        font-size: 1rem;
+    }
+
+
 `;
