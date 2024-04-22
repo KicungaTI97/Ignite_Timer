0a7c52f14c10d29cff9e7a5aeb822eaa5e5038d6
Joao Kicunga <https://github.com/KicungaTI97/Ignite_Timer>
2024-04-22 01:55:04 +0100

    Estrutura da aplicação - Configurando ESLint

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
 
-import { ThemeProvider } from "styled-components" 
-import { defaultTheme } from "./styles/themes/default"
-
-import { Button } from "./components/Button"
-import { GlobalStyle } from "./styles/themes/global"
+import { Button } from './components/Button'
+import { GlobalStyle } from './styles/themes/global'
 
 export function App() {
-
-
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant = "primary" />
-      <Button variant = "secondary" />
-      <Button variant = "success" />
-      <Button variant = "danger" />
-      <Button  />
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger" />
+      <Button />
 
-     <GlobalStyle />
-     
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
-
-
- 
